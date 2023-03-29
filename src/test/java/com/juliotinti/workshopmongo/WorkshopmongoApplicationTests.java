8756c043776dc39db3ed14458ca2d3474c83ea8d
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-29 10:30:00 -0300

    Started web project

---
+++ src/test/java/com/juliotinti/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliotinti.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
