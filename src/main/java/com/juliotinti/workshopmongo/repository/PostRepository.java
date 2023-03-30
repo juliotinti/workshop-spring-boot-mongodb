3c19ec1883f75134fe15b8f24eb8c709b5f4ebbf
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-30 11:52:20 -0300

    Implemente @Query for simple query

--- src/main/java/com/juliotinti/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/juliotinti/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.juliotinti.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.juliotinti.workshopmongo.domain.Post;
@@ -11,5 +12,9 @@ import com.juliotinti.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	//tiramos daqui https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
